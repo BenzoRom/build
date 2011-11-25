4cd07d550197ab96043471652aca0ac496f9a738
Ricardo Cerqueira <github@cerqueira.org>
2011-11-25 15:30:36 +0000

    build: Preliminary support for Custom product builds
    If not building a aosp_* product, skip crawling the filesystem for
    AndroidProduct makefiles (and, of course, parsing their contents)
    and aim directly for the vendor/custom/products/<product> device configuration
    
    Change-Id: I2a5e70dda973a6fcdbba0d5e26b35b99d3f1aea2
    
    Change how we search for Custom makefiles.
    
    build: Fix fastpath code for CUSTOM_BUILD
    * We need to adjust to the new validations
    
    PureNexus Edits:
    flipped the logic only crawl if its aosp_ assume everything else is a CUSTOM_BUILD
    
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -129,6 +129,14 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+
+    if (echo -n $1 | grep -q -e "^aosp_") ; then
+       CUSTOM_BUILD=
+    else
+       CUSTOM_BUILD=$1
+    fi
+    export CUSTOM_BUILD
+
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
@@ -609,6 +617,7 @@ function lunch()
         return 1
     fi
 
+    check_product $product
     TARGET_PRODUCT=$product \
     TARGET_BUILD_VARIANT=$variant \
     TARGET_PLATFORM_VERSION=$version \
