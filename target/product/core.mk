a90d30f1f2e53e0526c18a160bea294e23bc46dc
beanstown106 <nbenis106@gmail.com>
2015-10-17 06:16:04 -0400

    Drop deprecated stuff/move stuff to vendor/device
    *drop aosp messaging in favor of google messenger
    *drop deskclock in favor of googles
    *drop email and exchange in favor of googles
    *drop camera2 in favor of googles camera
    *drop deprecated aosp gallery2
    *drop deprecated aosp calendar
    *drop deprecated launcher2
    *drop deprecated music
    *drop deprecated picotts
    *drop deprecated quicksearchbox
    *drop deprecated sample home
    *drop deprecated openwnn keyboard
    *drop deprecated protips
    *drop deprecated video editor libraries
    *drop launcher 3 we will set in vendor
    *drop outdated wallpapers we will set livewallpaper picker in vendor
    *drop keyguard.no_require_sim=true we will set in vendor
    *drop dataroaming=true we will set this in vendor
    *drop apns-conf we will set this per device
    
    Change-Id: I907da38d3ededb1bf1d81c0371d90d00750b2edf
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- target/product/core.mk
+++ target/product/core.mk
@@ -24,17 +24,13 @@ PRODUCT_PACKAGES += \
     BasicDreams \
     BlockedNumberProvider \
     BookmarkProvider \
-    Browser2 \
     BuiltInPrintService \
-    Calendar \
     CalendarProvider \
     CaptivePortalLogin \
     CertInstaller \
     Contacts \
-    DeskClock \
     DocumentsUI \
     DownloadProviderUi \
-    Email \
     ExactCalculator \
     ExternalStorageProvider \
     FusedLocation \
@@ -42,16 +38,13 @@ PRODUCT_PACKAGES += \
     KeyChain \
     Keyguard \
     LatinIME \
-    Launcher2 \
     ManagedProvisioning \
     MtpDocumentsProvider \
-    PicoTts \
     PacProcessor \
     libpac \
     PrintSpooler \
     PrintRecommendationService \
     ProxyHandler \
-    QuickSearchBox \
     Settings \
     SharedStorageBackup \
     StorageManager \
