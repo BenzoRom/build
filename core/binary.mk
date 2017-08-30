96d416de3c1d7f9f8f49ba28dc41cc5635b5aa88
mydongistiny <jaysonedson@gmail.com>
2017-08-30 11:56:34 -0700

    dragontc: set up for clang6 and oreo [1/2]
    see soong for the rest
    
    Change-Id: I3db6870483e77bd22fe9b1dcf1f734b07154b64b
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/binary.mk
+++ core/binary.mk
@@ -441,6 +441,11 @@ ifneq (,$(my_cpp_std_version))
    my_cpp_std_cppflags := -std=$(my_cpp_std_version)
 endif
 
+# Include DragonTC Optimizations
+ifneq ($(DISABLE_DTC_OPTS),true)
+  include $(BUILD_SYSTEM)/dragontc.mk
+endif
+
 # arch-specific static libraries go first so that generic ones can depend on them
 my_static_libraries := $(LOCAL_STATIC_LIBRARIES_$($(my_prefix)$(LOCAL_2ND_ARCH_VAR_PREFIX)ARCH)) $(LOCAL_STATIC_LIBRARIES_$(my_32_64_bit_suffix)) $(my_static_libraries)
 my_whole_static_libraries := $(LOCAL_WHOLE_STATIC_LIBRARIES_$($(my_prefix)$(LOCAL_2ND_ARCH_VAR_PREFIX)ARCH)) $(LOCAL_WHOLE_STATIC_LIBRARIES_$(my_32_64_bit_suffix)) $(my_whole_static_libraries)
