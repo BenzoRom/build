e7bb60177e41572e637c7711d6d5bbba08a3eb28
mydongistiny <jaysonedson@gmail.com>
2017-10-11 01:18:07 -0700

    build: hardcode number of kernel jobs
    This hack forces how many jobs to use for the kernel since no matter what you put for -j X it always just runs 4
    I know there's a better way to fix this, but this works for now
    
    Change-Id: Id54c2ec083d2a5d39708c945ffa43370eb94117c
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/tasks/kernel.mk
+++ core/tasks/kernel.mk
@@ -70,7 +70,7 @@ MAKE_FLAGS :=
 
 ifeq ($(KERNEL_ARCH),arm64)
   # Avoid "unsupported RELA relocation: 311" errors (R_AARCH64_ADR_GOT_PAGE)
-  MAKE_FLAGS += CFLAGS_MODULE="-fno-pic"
+  MAKE_FLAGS += -j23 CFLAGS_MODULE="-fno-pic"
   ifeq ($(TARGET_ARCH),arm)
     KERNEL_CONFIG_OVERRIDE := CONFIG_ANDROID_BINDER_IPC_32BIT=y
   endif
