b1f23166a55736665c96232bb3d96f1445a09e2e
mydongistiny <jaysonedson@gmail.com>
2018-11-15 14:51:41 -0800

    build: Update for latest clang-8 [2/2]
    Change-Id: I25b6ca26e30fb3879b303a8b666da1b45cefa07e
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/dynamic_binary.mk
+++ core/dynamic_binary.mk
@@ -124,8 +124,8 @@ ifeq ($(my_use_clang_lld),true)
   $(strip_output): PRIVATE_OBJCOPY_ADD_SECTION := $(LLVM_OBJCOPY)
   $(strip_output): PRIVATE_STRIP := $(LLVM_STRIP)
   # GNU strip keeps .ARM.attributes section even with -strip-all,
-  # so here pass -keep=.ARM.attributes to llvm-strip.
-  $(strip_output): PRIVATE_STRIP_ALL_FLAGS := -strip-all -keep=.ARM.attributes
+  # so here pass -keep-section=.ARM.attributes to llvm-strip.
+  $(strip_output): PRIVATE_STRIP_ALL_FLAGS := -strip-all -keep-section=.ARM.attributes
 else
   $(strip_output): PRIVATE_OBJCOPY_ADD_SECTION := $($(LOCAL_2ND_ARCH_VAR_PREFIX)TARGET_OBJCOPY)
   $(strip_output): PRIVATE_STRIP := $($(LOCAL_2ND_ARCH_VAR_PREFIX)TARGET_STRIP)
