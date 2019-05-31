01b97586cccdb136d0f9258579fbc5f5661df8ef
Jason Edson <jaysonedson@gmail.com>
2019-05-31 14:42:56 -0700

    kernel: Use binutils from kernel toolchain with clang
    Change-Id: I8f017f215690ad1a33ee3d9e69c6343fb7e8abab
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- core/tasks/kernel.mk
+++ core/tasks/kernel.mk
@@ -176,6 +176,13 @@ ifeq ($(TARGET_KERNEL_CLANG_COMPILE),true)
     ifneq ($(TARGET_KERNEL_USE_LLD),)
         KERNEL_CC += LD=$(TARGET_KERNEL_CLANG_PATH)/bin/ld.lld
     endif
+    # Clang binutils
+    KERNEL_CC += AS=$(TARGET_KERNEL_CLANG_PATH)/bin/llvm-as
+    KERNEL_CC += AR=$(TARGET_KERNEL_CLANG_PATH)/bin/llvm-ar
+    KERNEL_CC += NM=$(TARGET_KERNEL_CLANG_PATH)/bin/llvm-nm
+    KERNEL_CC += OBJCOPY=$(TARGET_KERNEL_CLANG_PATH)/bin/llvm-objcopy
+    KERNEL_CC += OBJDUMP=$(TARGET_KERNEL_CLANG_PATH)/bin/llvm-objdump
+    KERNEL_CC += STRIP=$(TARGET_KERNEL_CLANG_PATH)/bin/llvm-strip
 endif
 
 ifneq ($(TARGET_KERNEL_MODULES),)
