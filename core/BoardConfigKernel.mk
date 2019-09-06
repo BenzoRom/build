73026d7f19812f6743b1174d2ff844f07fb86cdc
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-06 10:44:50 -0700

    kernel: Use prebuilt binaries
    perl, lz4c, and a bunch of other binaries are not allowed anymore.
    So we need to provide our own.
    
    Change-Id: Idbc88b76247b3dda7c5d52e5eed2d548c33f6f2d
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/BoardConfigKernel.mk
+++ core/BoardConfigKernel.mk
@@ -122,6 +122,11 @@ else
   KERNEL_MAKE_FLAGS += HOSTLDFLAGS="-L $(BUILD_TOP)/prebuilts/openssl/$(HOST_OS)-x86/1.1.1/lib/x86_64-linux-gnu"
 endif
 
+TOOLS_PATH_OVERRIDE := \
+    PATH=$(BUILD_TOP)/prebuilts/build-tools/$(HOST_OS)-x86/bin:$$PATH \
+    LD_LIBRARY_PATH=$(BUILD_TOP)/prebuilts/build-tools/$(HOST_OS)-x86/lib:$$LD_LIBRARY_PATH \
+    PERL5LIB=$(BUILD_TOP)/prebuilts/build-tools/common/perl-base
+
 # Set use the full path to the make command
 KERNEL_MAKE_CMD := $(BUILD_TOP)/prebuilts/build-tools/$(HOST_OS)-x86/bin/make
 
