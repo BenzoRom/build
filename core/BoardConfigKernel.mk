06edd17551b244a50e0f4a5bf77523a77a5c5b18
mydongistiny <jaysonedson@gmail.com>
2019-09-08 19:32:11 -0700

    kernel: Use system ccache if set for build
    Also update prebuilt ccache location
    
    Change-Id: Id177f467a8aac5fab4ec40c2e9082cdc11249190
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/BoardConfigKernel.mk
+++ core/BoardConfigKernel.mk
@@ -75,13 +75,13 @@ endif
 endif
 
 ifneq ($(USE_CCACHE),)
-    # Detect if the system already has ccache installed to use instead of the prebuilt
-    CCACHE_BIN := $(shell which ccache)
-
-    ifeq ($(CCACHE_BIN),)
-        CCACHE_BIN := $(BUILD_TOP)/prebuilts/misc/$(HOST_PREBUILT_TAG)/ccache/ccache
+    ifeq ($(USE_SYSTEM_CCACHE),)
+        CCACHE_BIN := $(BUILD_TOP)/prebuilts/build-tools/$(HOST_PREBUILT_TAG)/bin/ccache
         # Check that the executable is here.
-        CCACHE_BIN := $(strip $(wildcard $(CCACHE_BIN)))
+        CCACHE_BIN := $(strip $(wildcard $(ccache)))
+    else
+        # Detect if the system already has ccache installed to use instead of the prebuilt
+        CCACHE_BIN := $(shell which ccache)
     endif
 endif
 
