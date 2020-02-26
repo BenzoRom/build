822a27da9a19cec7c7043fd452524a00023c6bae
mydongistiny <jaysonedson@gmail.com>
2020-02-25 23:55:48 -0800

    Fix polly when building with latest clang-11
    Change-Id: Icbac2dc36087a83cf454f2100d6e1b49a516edd6
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/clang/custom_clang.mk
+++ core/clang/custom_clang.mk
@@ -23,7 +23,7 @@ POLLY := -O3 \
   -mllvm -polly-omp-backend=LLVM \
   -mllvm -polly-opt-fusion=max \
   -mllvm -polly-opt-simplify-deps=no \
-  -mllvm -polly-position=before-vectorizer \
+  -mllvm -polly-position=after-loopopt \
   -mllvm -polly-run-dce \
   -mllvm -polly-run-inliner \
   -mllvm -polly-vectorizer=stripmine \
