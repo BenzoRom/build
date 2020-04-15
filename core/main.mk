0a5dec8462d1dab05316e824b4997cc16925f8a1
mydongistiny <jaysonedson@gmail.com>
2020-04-14 17:56:51 -0700

    build: adb insecure on userdebug builds
    Change-Id: I3475818cc2ba2e921c355ddf36c41a4d5d4561ff
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/main.mk
+++ core/main.mk
@@ -278,11 +278,14 @@ ifneq (,$(user_variant))
 
   ifeq ($(user_variant),user)
     ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
+
   endif
 
   ifeq ($(user_variant),userdebug)
     # Pick up some extra useful tools
     tags_to_install += debug
+    # Allow logcat on first boot
+    ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
   else
     # Disable debugging in plain user builds.
     enable_target_debugging :=
