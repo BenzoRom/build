4aa7b786247f7d871ac3e4bdf97c3da284bcb08e
Luca Stefani <luca.stefani.ge1@gmail.com>
2020-08-02 19:08:31 +0200

    kernel: Rework how DTB/DTBO are built
    * Remove the separate dtbo.mk task and merge it
      in kernel.mk
    * Merge in-kernel and separated DTBO image generation
    
    Change-Id: I2cae84095cbd3d68abbe1899a9cae34c8a1e5fc9
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- core/BoardConfigKernel.mk
+++ core/BoardConfigKernel.mk
@@ -123,6 +123,11 @@ TOOLS_PATH_OVERRIDE := \
     LD_LIBRARY_PATH=$(BUILD_TOP)/prebuilts/build-tools/$(HOST_OS)-x86/lib:$$LD_LIBRARY_PATH \
     PERL5LIB=$(BUILD_TOP)/prebuilts/build-tools/common/perl-base
 
+# Set DTBO image locations so the build system knows to build them
+ifeq (true,$(filter true, $(TARGET_NEEDS_DTBOIMAGE) $(BOARD_KERNEL_SEPARATED_DTBO)))
+BOARD_PREBUILT_DTBOIMAGE ?= $(TARGET_OUT_INTERMEDIATES)/DTBO_OBJ/arch/$(KERNEL_ARCH)/boot/dtbo.img
+endif
+
 # Set use the full path to the make command
 KERNEL_MAKE_CMD := $(BUILD_TOP)/prebuilts/build-tools/$(HOST_OS)-x86/bin/make
 
@@ -150,13 +155,6 @@ OUT_DIR_PREFIX := $(shell echo $(OUT_DIR) | sed -e 's|/target/.*$$||g')
 KERNEL_BUILD_OUT_PREFIX :=
 ifeq ($(OUT_DIR_PREFIX),out)
 KERNEL_BUILD_OUT_PREFIX := $(BUILD_TOP)/
-endif
-
-# Set DTBO image locations so the build system knows to build them
-ifeq ($(TARGET_NEEDS_DTBOIMAGE),true)
-BOARD_PREBUILT_DTBOIMAGE ?= $(PRODUCT_OUT)/dtbo/arch/$(KERNEL_ARCH)/boot/dtbo.img
-else ifeq ($(BOARD_KERNEL_SEPARATED_DTBO),true)
-BOARD_PREBUILT_DTBOIMAGE ?= $(PRODUCT_OUT)/dtbo-pre.img
 endif
 
 include build/make/core/BoardConfigSoong.mk
