89d81ebe5599783688fdf7a1850c0f5240fd0418
Jason Edson <jaysonedson@gmail.com>
2021-03-02 13:26:18 -0800

    kernel: Pass CROSS_COMPILE_COMPAT for vdso
    Redbull/bramble depend on it to build compat vdso:
    https://github.com/aosp-mirror/kernel_msm/commit/233360c3b481fb911e828ba6cdba26fb475e56d4
    
    Change-Id: I87fc508d58ff10952a4cf52a7b5b7aa01b87921d
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- core/BoardConfigKernel.mk
+++ core/BoardConfigKernel.mk
@@ -96,7 +96,8 @@ endif
 
 # Needed for CONFIG_COMPAT_VDSO, safe to set for all arm64 builds
 ifeq ($(KERNEL_ARCH),arm64)
-   KERNEL_CROSS_COMPILE += CROSS_COMPILE_ARM32="$(KERNEL_TOOLCHAIN_arm)/$(KERNEL_TOOLCHAIN_PREFIX_arm)"
+  CC_ARM32_PATH=$(KERNEL_TOOLCHAIN_arm)/$(KERNEL_TOOLCHAIN_PREFIX_arm)
+  KERNEL_CROSS_COMPILE += CROSS_COMPILE_ARM32="$(CC_ARM32_PATH)" CROSS_COMPILE_COMPAT="$(CC_ARM32_PATH)"
 endif
 
 # Clear this first to prevent accidental poisoning from env
